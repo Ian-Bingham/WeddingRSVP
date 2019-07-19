572c8d8545243548cbcf965d6b6ba3ab44367351
Ian <ianbingham24@gmail.com>
2019-07-18 21:37:26 -0700

    lower case rsvp (code side)

--- ui/src/pages/Home.jsx
+++ ui/src/pages/Home.jsx
@@ -1,16 +1,14 @@
 import React, { useState } from 'react'
 
-const RSVPForm = () => {
-  const [RSVPAnswer, setRSVPAnswer] = useState('no')
+const RsvpForm = () => {
+  const [rsvpAnswer, setRsvpAnswer] = useState('no')
 
-  const onRSVPChange = (evt) => {
-    setRSVPAnswer(evt.target.value)
+  const onRsvpChange = (evt) => {
+    setRsvpAnswer(evt.target.value)
   }
 
   const onSubmit = (evt) => {
     evt.preventDefault()
-
-    console.log(RSVPAnswer)
   }
 
   return (
@@ -19,19 +17,20 @@ const RSVPForm = () => {
       <div className='rsvpOptions'>
         <input 
           type='radio' 
-          name='RSVP' 
+          name='rsvp' 
           value='no' 
-          onChange={onRSVPChange}
-          checked={RSVPAnswer === 'no'} 
+          onChange={onRsvpChange}
+          checked={rsvpAnswer === 'no'} 
         /> No
         <input 
           type='radio' 
-          name='RSVP' 
+          name='rsvp' 
           value='yes' 
-          onChange={onRSVPChange}
-          checked={RSVPAnswer === 'yes'} 
+          onChange={onRsvpChange}
+          checked={rsvpAnswer === 'yes'} 
         /> Yes
       </div>
+      {}
       <input type='submit' value='Submit'/>
     </form>
   )
@@ -45,7 +44,7 @@ const Home = () => {
       <div className='homeMain'>
         <h2>Welcome, Name</h2>
           <button className='rsvpButton' onClick={() => setModalOpen(true)}>RSVP Now</button>
-          {modalOpen ? <RSVPForm /> : null}
+          {modalOpen ? <RsvpForm /> : null}
       </div>
     </>
   )
